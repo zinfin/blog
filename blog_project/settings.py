73bb98c5794ad59df796a1bd86a142a90c151eee
zinfin <sandiepeters@comcast.net>
2018-12-04 15:23:30 -0800

    Heroku config files and updates

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rjti%*q0&feqto^iy&&dep)he1c%jp63bp$!a%0x+a=y^f#w&8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 LOGIN_REDIRECT_URL = 'home'
 
